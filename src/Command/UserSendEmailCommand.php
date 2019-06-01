9c0e5d0366927d88c02aead941571145856a9632
Evgenii Ryshkov <eryshkov@gmail.com>
2019-06-01 20:12:58 +0300

    UserSendEmailCommand.php fixed

--- src/Command/UserSendEmailCommand.php
+++ src/Command/UserSendEmailCommand.php
@@ -2,32 +2,53 @@
 
 namespace App\Command;
 
+use App\Repository\UserRepository;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
 class UserSendEmailCommand extends Command
 {
     protected static $defaultName = 'user:send:email';
-
+    /**
+     * @var UserRepository
+     */
+    private $userRepository;
+    
+    /**
+     * UserSendEmailCommand constructor.
+     */
+    public function __construct(UserRepository $userRepository)
+    {
+        parent::__construct(null);
+        $this->userRepository = $userRepository;
+    }
+    
     protected function configure()
     {
         $this
             ->setDescription('Adds user to email queue')
-            ->addArgument('user', InputArgument::REQUIRED, 'User which receive an email')
+            ->addArgument('email', InputArgument::REQUIRED, 'User\'s email')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $io = new SymfonyStyle($input, $output);
-        $userArg = $input->getArgument('user');
+        $userEmail = $input->getArgument('email');
 
-        if ($userArg) {
-            $io->note(sprintf('You passed an user: %s', $userArg));
+        if ($userEmail) {
+            $io->note(sprintf('You passed an user\'s email: %s', $userEmail));
+        }
+    
+        $user = $this->userRepository->findOneBy([
+            'email' => $userEmail,
+        ]);
+    
+        if (!isset($user)) {
+            $io->error('User with ' . $userEmail . ' not found!');
         }
 
         $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
